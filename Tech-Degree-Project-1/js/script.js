ec32895ac9136ec0c22d3b74852b72d94c38acea
papageg <greggordon98@gmail.com>
2018-09-10 12:00:24 -0700

    Issue with  category

--- Tech-Degree-Project-1/js/script.js
+++ Tech-Degree-Project-1/js/script.js
@@ -1,68 +1,76 @@
 // FSJS - Random Quote Generator
 //The object literal array that holds all 5 or more quotes.
 // Create the array of quote objects and name it quotes
+let red;
+let blue;
+let green;
+
 let quotes = [
   {
-    quote:"“Don't cry because it's over, smile because it happened.”",
+    quote:"Don't cry because it's over, smile because it happened.",
     name:"Dr. Seuss",
-    category:"attributed-no-source, cry, crying, experience, happiness, joy, life, misattributed-dr-seuss, optimism, sadness, smile, smiling",
+    category:"happiness, joy, life, optimism, sadness, smile, smiling",
   },
   {
-    quote:"“Be yourself; everyone else is already taken.”",
+    quote:"Be yourself; everyone else is already taken.",
     name:"Oscar Wilde",
-    category:"attributed-no-source, be-yourself, honesty, inspirational, misattributed-oscar-wilde",
+    category:"be yourself, honesty, inspirational",
   },
   {
-    quote:"“Anyone who has never made a mistake has never tried anything new.”",
+    quote:"Anyone who has never made a mistake has never tried anything new.",
     name:"Albert Einstein",
-    category:"attributed-no-source, mistakes 16554 likes Like",
+    category:"mistakes",
   },
   {
-    quote:"“It takes courage to grow up and become who you really are.”",
+    quote:"It takes courage to grow up and become who you really are.",
     name:"E.E. Cummings",
-    category:"attributed-no-source, courage",
+    category:"courage",
   },
   {
-    quote:"“Never underestimate the power of stupid people in large groups.”",
+    quote:"Never underestimate the power of stupid people in large groups.",
     name:"George Carlin",
-    category:"attributed-no-source, human-nature, humor, misattributed-george-carlin, stupidity",
+    category:"human-nature, humor, misattributed-george-carlin, stupidity",
   },
   {
-    quote:"“If you're afraid of butter, use cream.”",
+    quote:"If you're afraid of butter, use cream.",
     name:"Julia Child",
-    category:"attributed-no-source, cholesterol, food",
+    category:"cholesterol, food",
   },
   {
-    quote:"“Anyone can find the switch after the lights are on.”",
+    quote:"Anyone can find the switch after the lights are on.",
     name:"Confucius",
-    category:"attributed-no-source",
+    category:"Philosopher",
   }
 ];
 /*
-  GOT ALL THE QUOTES FROM https://www.goodreads.com/quotes/tag/attributed-no-source?page=1
+  GOT ALL THE QUOTES FROM
+  https://www.goodreads.com/quotes/tag/attributed-no-source?page=1
 */
 //END OF QUOTES
 
 
-// Create the getRandomQuote function and name it getRandomQuote
-function getRandomQuote (quo) {
-  let quo_quotes = [];
-  for (i = 0; i < quo.length; i+=1) {
-    quo_quotes.push(quo[i].quote);
-  }
-  return quo_quotes[Math.floor(Math.random() * i)];
+// Created the getRandomQuote function and named it getRandomQuote.
+function getRandomQuote() {
+  let quo = Math.floor(Math.random() * quotes.length);
+  let ranQuo = quotes[quo];
+  return ranQuo;
 }
-console.log(getRandomQuote(quotes));
+//getRandomQuote creates a function that gets random number and selects a quote then returns the quote to the function.
 
-// Create the printQuote funtion and name it printQuote
-function printQuote () {
-  let print_quote = [];
-  for (i = 0; i < print_quote.length; i += 1){
 
-  }
+// Created the printQuote funtion and named it printQuote.
+function printQuote() {
+  let getQuote = getRandomQuote();
+  let sendHtml = '<p class="quote">'+ getQuote.quote +'</p>';
+  sendHtml += '<p class="source">'+ getQuote.name + '</p>';
+  sendHtml += '<p class="catagor">'+ getQuote.category + '</p>';
+  document.getElementById("quote-box").innerHTML = sendHtml;
 }
+/* printQuote function will use the getRandomQuote function and add
+it the the paragrah with the class of quote and the name to the source class
+then adding the category.
 
-
+*/
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
